d785474709eb67decd4c04a3537a1ea810184153
Gikubu <davidndungu8203@gmail.com>
2024-02-26 20:04:30 +0300

    "here i added new line break for the user"

--- bandnamegenerator.dart
+++ bandnamegenerator.dart
@@ -4,17 +4,12 @@ void main() {
   //1. Create a greeting for your program.
   print("hello welcome to the band generator app.");
   //2. Ask the user for the city that they grew up in.
-  print("enter city name ");
+  print("enter city name \n");
   String? cityName = stdin.readLineSync();
   //3. Ask the user for the name of a pet.
-  print("enter a pet's name :");
+  print("enter a pet's name : \n");
   String? petName = stdin.readLineSync();
   //4. Combine the name of their city and pet and show them their band name.
+  print("your band name is : ${cityName}${petName}");
+  //5. Make sure the input cursor shows on a new line:
 }
-
-
-
-
-
-
-//5. Make sure the input cursor shows on a new line:
