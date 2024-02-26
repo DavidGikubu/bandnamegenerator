6c1d5e03efa5fe0e863218718b8cf08aa6aed205
Gikubu <davidndungu8203@gmail.com>
2024-02-26 19:33:21 +0300

    "here i added some code to hold user input"

--- bandnamegenerator.dart
+++ bandnamegenerator.dart
@@ -1,11 +1,20 @@
-//1. Create a greeting for your program.
+import 'dart:io';
+
 void main() {
+  //1. Create a greeting for your program.
   print("hello welcome to the band generator app.");
+  //2. Ask the user for the city that they grew up in.
+  print("enter city name ");
+  String? cityName = stdin.readLineSync();
+  //3. Ask the user for the name of a pet.
+  print("enter a pet's name :");
+  String? petName = stdin.readLineSync();
+  //4. Combine the name of their city and pet and show them their band name.
 }
-//2. Ask the user for the city that they grew up in.
 
-//3. Ask the user for the name of a pet.
 
-//4. Combine the name of their city and pet and show them their band name.
+
+
+
 
 //5. Make sure the input cursor shows on a new line:
